248cdba08803e9b9dfbbfc67f3e58dd40c84e4ac
Nerwosolek <subtraction_game@gieorgijewski.pl>
2021-07-16 20:43:44 +0200

    Solution for exercise 2-08

---
+++ src/ex2.08/rightrot.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+unsigned
+rightrot(unsigned x, int n)
+{
+    int carry;
+    unsigned int mask = 1;
+    unsigned int sig1 = ~(~0u >> 1);
+    // printf("sig1 = %u\n", sig1);
+    // printf("sig1 = %u\n", 1 << 16);
+    // printf("sig1 * 1 = %u\n", sig1 * 1);
+    // printf("sig1 * 0 = %u\n", sig1 * 0);
+    for (size_t i = 0; i < n; i++)
+    {
+        carry = x & mask;
+        x = x >> 1;
+        x += sig1 * carry;
+    }
+    return x;
+}
+
+int
+main(void)
+{
+    printf("5 ROR 1 = %u\n", rightrot(5u,1));
+    unsigned expected = ~(~0u >> 1);
+    expected += ~(~0u >> 1) >> 1;
+    expected += ~(~0u >> 1) >> 2;
+    expected += 127u >> 3;
+    printf("127 ROR 3 = %u, expected = %u\n", rightrot(127u, 3), expected);
+    expected = ~(~0u >> 1) + (~(~0u >> 1) >> 3) + (~(~0u >> 1) >> 4) + 2;
+    printf("166 ROR 6 = %u, expected = %u\n", rightrot(166u, 6), expected);
+    return 0;
+}
